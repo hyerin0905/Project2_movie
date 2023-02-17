30a60897b9f6ebf1a48adf3410d05f1927961435
hyerin <lwh39@naver.com>
2023-02-17 14:49:58 +0900

    예매 내역 확인 페이지(2월17일 금)

--- src/js/TicketingCheckList.js
+++ src/js/TicketingCheckList.js
@@ -1,30 +1,39 @@
-import '../css/style.css';
+import '../css/ticketing_2.css';
 
-function TicketingCheckList () {
+function TicketingCheckList() {
     return (
         <>
             <div className="confirmation">
-                <div className="box1">
-                    <h2>예매내역 확인</h2>
-                    <div className="box2 cf">
-                        <img src="https://via.placeholder.com/334" />
-                        <ul>
-                            <li>예매 영화: </li>
-                            <li>관람 일시: </li>
-                            <li>관람 좌석: </li>
-                            <li>이름: </li>
-                            <li>휴대번호: </li>
-                        </ul>
+                <div className="box1 ">
+                    <div className='box1-1'>
+                        <h2>예매내역 확인</h2>
+                        <div className="box2 cf">
+                            <img src="https://via.placeholder.com/334" />
+                            <ul>
+                                <li>예매 영화:  </li>
+                                <br />
+                                <li>관람 일시: </li>
+                                <br />
+                                <li>관람 좌석: </li>
+                                <br />
+                                <hr style={{width:500}}/>
+                                <br />
+                                <li>이름: </li>
+                                <br />
+                                <li>휴대번호: </li>
+                            </ul>
+                        </div>
+                        <button className="confirmation-btn">확인</button>
                     </div>
                 </div>
-                <button className="confirmation-btn">확인</button>
-                
-                
-                
+
+
+
+
 
 
             </div>
-        
+
         </>
     );
 }
